2ad403d40a87cc80dd7a1312d09ba4a46f641d70
Kim Inseo <sano307@naver.com>
2020-04-16 01:47:11 +0900

    Add the todolistRestApi for GET mock response

--- lib/todolist-serverless-stack.ts
+++ lib/todolist-serverless-stack.ts
@@ -1,5 +1,6 @@
+import { EmptyModel, MockIntegration, PassthroughBehavior, RestApi } from '@aws-cdk/aws-apigateway';
+import { UserPool } from '@aws-cdk/aws-cognito';
 import * as cdk from '@aws-cdk/core';
-import { UserPool } from '@aws-cdk/aws-cognito'
 
 export class TodolistServerlessStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,39 @@ export class TodolistServerlessStack extends cdk.Stack {
       signInAliases: {
         email: true
       }
+    });
+
+    const todolistRestApi = new RestApi(this, 'todolistRestApi', {
+      restApiName: 'Todolist API'
+    });
+    todolistRestApi.root.addMethod("GET", new MockIntegration({
+      integrationResponses: [{
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Access-Control-Allow-Headers":
+            "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+          "method.response.header.Access-Control-Allow-Origin": "'*'",
+          "method.response.header.Access-Control-Allow-Credentials": "'false'",
+          "method.response.header.Access-Control-Allow-Methods": "'GET'",
+        }
+      }],
+      passthroughBehavior: PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": "{\"statusCode\": 200}"
+      }
+    }), {
+      methodResponses: [{
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Access-Control-Allow-Headers": true,
+          "method.response.header.Access-Control-Allow-Origin": true,
+          "method.response.header.Access-Control-Allow-Credentials": true,
+          "method.response.header.Access-Control-Allow-Methods": true,
+        },
+        responseModels: {
+          "application/json": new EmptyModel()
+        },
+      }]
     })
   }
 }
