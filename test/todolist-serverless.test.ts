d4593a6616634416af47c25accf2ff23f21ae7cb
Kim Inseo <sano307@naver.com>
2020-04-16 01:06:04 +0900

    Default test case

--- test/todolist-serverless.test.ts
+++ test/todolist-serverless.test.ts
@@ -1,13 +1,11 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import TodolistServerless = require('../lib/todolist-serverless-stack');
+import { SynthUtils } from '@aws-cdk/assert';
+import { App } from '@aws-cdk/core';
+import { TodolistServerlessStack } from '../lib/todolist-serverless-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new TodolistServerless.TodolistServerlessStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+describe('todolist', () => {
+  test('default', () => {
+    const app = new App();
+    const stack = new TodolistServerlessStack(app, "TestTodolistServerlessStack");
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
 });
